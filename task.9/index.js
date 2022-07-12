79c0f5c0a96361d05128e964d39914b1beb5c290
roccaccu <naimadzhun@gmail.com>
2022-07-12 20:40:24 +0800

    secnd

--- task.9/index.js
+++ task.9/index.js
@@ -1,1 +1,21 @@
-function sumFinder() {}
+function sumFinder(numbers, sum) {
+	let count = 0
+	let numbersOfSum = []
+	for (let i = 0; i < numbers.length; i++) {
+		count += numbers[i]
+		numbersOfSum.push(numbers[i])
+	}
+	if (count === sum) {
+		return numbersOfSum
+	} else {
+		count = 0
+		numbersOfSum = []
+		for (let i = 0; i < numbers.length; i++) {
+			let newArray = numbers.slice(0, i).push(numbers.slice(i + 1))
+			sumFinder(newArray, sum)
+		}
+		return false
+	}
+}
+
+console.log(sumFinder([1, 2, 3], 5))
